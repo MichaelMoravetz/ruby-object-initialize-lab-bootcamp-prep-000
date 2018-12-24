3c9b1a456591cdf48fcbd603aa51cf0d2eb3ed02
Michael Moravetz <moravemv@gmail.com>
2018-12-24 23:59:15 +0000

    Automatically backed up by Learn

---
+++ lib/person.rb
@@ -0,0 +1,19 @@
+class Person
+  def initialize(name)
+    @name = name
+  end
+  
+  def name=(name)
+    @name = name
+  end
+  def name
+    @name
+  end
+end
+
+class Dog 
+  def initialize(name, breed = "mutt")
+    @breed = breed
+  end
+  
+  def
